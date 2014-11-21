f06c39170c99002dd5a9fec74b6c1071d481fd1e
Mercurion <jackbianchini@gmail.com>
2014-11-21 13:41:10 +0100

    Commit after second activity
    this is a commit

---
+++ app/src/main/java/polimi/mancala/MatchActivity.java
@@ -0,0 +1,10 @@
+package polimi.mancala;
+
+import android.app.Activity;
+
+/**
+ * Created by jack on 21/11/2014.
+ */
+public class MatchActivity extends Activity {
+
+}
---
+++ app/src/main/java/polimi/mancala/User.java
@@ -0,0 +1,7 @@
+package polimi.mancala;
+
+/**
+ * Created by jack on 21/11/2014.
+ */
+public class User {
+}
