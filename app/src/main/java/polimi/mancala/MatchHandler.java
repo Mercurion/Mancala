2dce1a88bd448c2abe70b9acd1aac454cd7c15be
Mercurion <jackbianchini@gmail.com>
2014-11-21 15:10:22 +0100

    commit

--- app/src/main/java/polimi/mancala/MatchHandler.java
+++ app/src/main/java/polimi/mancala/MatchHandler.java
@@ -3,5 +3,6 @@ package polimi.mancala;
 /**
  * Created by jack on 21/11/2014.
  */
-public class Match {
+public class MatchHandler {
+
 }
--- app/src/main/java/polimi/mancala/User.java
+++ app/src/main/java/polimi/mancala/User.java
@@ -4,4 +4,5 @@ package polimi.mancala;
  * Created by jack on 21/11/2014.
  */
 public class User {
+
 }
