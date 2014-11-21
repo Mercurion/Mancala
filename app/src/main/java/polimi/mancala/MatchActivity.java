61b1c6602c3c2e9ed5e679231a69d02b7e3aba49
Mercurion <jackbianchini@gmail.com>
2014-11-21 14:30:41 +0100

    commit

--- app/src/main/java/polimi/mancala/MatchActivity.java
+++ app/src/main/java/polimi/mancala/MatchActivity.java
@@ -1,10 +1,18 @@
 package polimi.mancala;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by jack on 21/11/2014.
  */
 public class MatchActivity extends Activity {
 
+    @Override
+    protected void onCreate (Bundle SavedInstanceState) {
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.activity_match);
+    }
+
+    /* this is a comment */
 }
