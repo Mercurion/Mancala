ac0c1323f3ee15043ec38b1b3bc2ac9179ba71b8
Mercurion <jackbianchini@gmail.com>
2014-11-28 21:35:27 +0100

    Commit at the end of Game Logic. All the constraints are consistent. Minor Bug solved

---
+++ app/src/androidTest/java/polimi/mancala/TableTest.java
@@ -0,0 +1,44 @@
+package polimi.mancala;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by jack on 28/11/2014.
+ */
+public class TableTest extends InstrumentationTestCase {
+
+    TableHandler tableToTest = new TableHandler();
+
+    public void testInitialTable () {
+        tableToTest.initializeGameBoard();
+        assertEquals(tableToTest.getNumOfSeeds(0),3);
+        assertEquals(tableToTest.getNumOfSeeds(1),3);
+        assertEquals(tableToTest.getNumOfSeeds(2),3);
+        assertEquals(tableToTest.getNumOfSeeds(3),3);
+        assertEquals(tableToTest.getNumOfSeeds(4),3);
+        assertEquals(tableToTest.getNumOfSeeds(5),3);
+        assertEquals(tableToTest.getNumOfSeeds(6),0);
+        assertEquals(tableToTest.getNumOfSeeds(7),3);
+        assertEquals(tableToTest.getNumOfSeeds(8),3);
+        assertEquals(tableToTest.getNumOfSeeds(9),3);
+        assertEquals(tableToTest.getNumOfSeeds(10),3);
+        assertEquals(tableToTest.getNumOfSeeds(11),3);
+        assertEquals(tableToTest.getNumOfSeeds(12),3);
+        assertEquals(tableToTest.getNumOfSeeds(13),0);
+    }
+
+
+    public void testAddSeed () {
+        tableToTest.initializeGameBoard();
+        tableToTest.addSeed(5);
+        assertEquals(4,tableToTest.getNumOfSeeds(5));
+    }
+
+    public void testClearBowl () {
+        tableToTest.initializeGameBoard();
+        tableToTest.clearBowls(4);
+        assertEquals(0,tableToTest.getNumOfSeeds(4));
+    }
+
+
+}
