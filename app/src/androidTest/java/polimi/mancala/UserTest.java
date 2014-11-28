005244fce7eb68cdb7ab442106ef43332224a237
Mercurion <jackbianchini@gmail.com>
2014-11-28 18:19:47 +0100

    Commit at the end of Game Logic. All the constraints are consistent. Minor Bug solved

---
+++ app/src/androidTest/java/polimi/mancala/UserTest.java
@@ -0,0 +1,15 @@
+package polimi.mancala;
+
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void testGetScore() throws Exception {
+
+    }
+}
