3182b5745cac75ab3305d485abd275c1eca6ed2a
Mercurion <jackbianchini@gmail.com>
2014-12-19 12:31:28 +0100

    other methods

--- app/src/main/java/polimi/mancala/Bowl.java
+++ app/src/main/java/polimi/mancala/Bowl.java
@@ -11,4 +11,20 @@ public class Bowl {
 
     int index;
     int numOfSeed;
+
+    public void setIndex (int index) {
+        this.index = index;
+    }
+
+    public int getIndex () {
+        return index;
+    }
+
+    public void setNumOfSeed (int seeds) {
+        this.numOfSeed = seeds;
+    }
+
+    public int getNumOfSeed () {
+        return numOfSeed;
+    }
 }
