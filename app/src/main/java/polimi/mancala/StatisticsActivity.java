c95b27558d1713fe3cf58ab2c590e424046196f0
Mercurion <jackbianchini@gmail.com>
2015-01-09 19:25:49 +0100

    Giacomo Bianchini
    Little new methods

--- app/src/main/java/polimi/mancala/StatisticsActivity.java
+++ app/src/main/java/polimi/mancala/StatisticsActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by jack on 26/11/2014.
@@ -24,10 +25,22 @@ public class StatisticsActivity extends Activity {
         setContentView(R.layout.activity_statistics);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         Button goHome = (Button) findViewById(R.id.goHomeButton);
+        final Button animButt = (Button) findViewById(R.id.animButton);
+        final TextView text = (TextView) findViewById(R.id.textView);
         int x = 0;
+//        x = text.getHeight();
         int y = 0;
-        Animation traslation = new TranslateAnimation(x, x+3, y, y+3);
+//        y = text.getWidth();
+        final Animation traslation = new TranslateAnimation(x, x+100, y, y+100);
+        traslation.setDuration(2000);
+        traslation.getFillAfter();
 
+        animButt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                text.startAnimation(traslation);
+            }
+        });
         goHome.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Do something in response to button click
