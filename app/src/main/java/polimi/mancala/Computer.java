ef2c6ab4c6e880b5325bd05e8606d48b69b9825e
Mercurion <jackbianchini@gmail.com>
2015-01-10 17:38:22 +0100

    Giacomo Bianchini
    Created the computer class. use in the definition of a H-C game

---
+++ app/src/main/java/polimi/mancala/Computer.java
@@ -0,0 +1,17 @@
+package polimi.mancala;
+
+/**
+ * Created by jack on 10/01/2015.
+ */
+public class Computer extends User {
+
+
+    public Computer (int id) {
+       super(id);
+    }
+
+
+    public void getBestMove (TableHandler board) {
+
+    }
+}
