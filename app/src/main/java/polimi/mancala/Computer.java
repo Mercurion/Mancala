37c6f347dbfa6680aa3cf1b47df63d62eed292b3
Mercurion <jackbianchini@gmail.com>
2015-01-10 18:42:00 +0100

    Giacomo Bianchini
    Created the computer class. use in the definition of a H-C game

--- app/src/main/java/polimi/mancala/Computer.java
+++ app/src/main/java/polimi/mancala/Computer.java
@@ -9,23 +9,23 @@ import java.util.Iterator;
 public class Computer extends User {
 
 
-    public Computer (int id) {
-       super(id);
+    public Computer(int id) {
+        super(id);
     }
 
 
-    public Integer getStealAndPush (TableHandler board) { //check if i can do a steal&push
+    public Integer getStealAndPush(TableHandler board) { //check if i can do a steal&push
         int i, seed, j;
-        ArrayList <Integer>  candidate = new ArrayList();
+        ArrayList<Integer> candidate = new ArrayList();
         Container c = board.getContainerByIndex(0);
-        for (i=0; i<14; i++) {
+        for (i = 0; i < 14; i++) {
             if (c.getOwnerId().equals(this.getId())) {
-                seed=c.getNumSeeds();
+                seed = c.getNumSeeds();
                 Container temp = c;
-                for (j=0; j< seed; j++)
+                for (j = 0; j < seed; j++)
                     temp = temp.getNextContainer();
 
-                if (temp.getNumSeeds()==0 && temp.getOwnerId().equals(this.getId()))
+                if (temp.getNumSeeds() == 0 && temp.getOwnerId().equals(this.getId()))
                     candidate.add(c.getIndex());
 
 
@@ -34,18 +34,18 @@ public class Computer extends User {
 
         if (candidate.isEmpty()) {
             return -1;
-        }
-            else {
-                ArrayList <Integer> numOppositeSeeds = new ArrayList();
-            for (i=0; i<candidate.size(); i++)
+        } else {
+            ArrayList<Integer> numOppositeSeeds = new ArrayList();
+            for (i = 0; i < candidate.size(); i++)
                 numOppositeSeeds.add(i, board.getNumOfSeedByIndex(12 - candidate.get(i)));
-            Integer max =0;
-            int index =0;
-            for (i=0; i< numOppositeSeeds.size(); i++)
-                if (numOppositeSeeds.get(i) >= max ) {
+            Integer max = 0;
+            int index = 0;
+            for (i = 0; i < numOppositeSeeds.size(); i++)
+                if (numOppositeSeeds.get(i) >= max) {
                     max = numOppositeSeeds.get(i);
                     index = i;
                 }
             return candidate.get(index);
         }
+    }
 }
