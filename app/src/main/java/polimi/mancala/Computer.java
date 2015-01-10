5b5bb011567c1e0a6f6563b9df72a6932bd659e9
Mercurion <jackbianchini@gmail.com>
2015-01-10 18:41:24 +0100

    Giacomo Bianchini
    Created the computer class. use in the definition of a H-C game

--- app/src/main/java/polimi/mancala/Computer.java
+++ app/src/main/java/polimi/mancala/Computer.java
@@ -1,5 +1,8 @@
 package polimi.mancala;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * Created by jack on 10/01/2015.
  */
@@ -11,7 +14,38 @@ public class Computer extends User {
     }
 
 
-    public void getBestMove (TableHandler board) {
+    public Integer getStealAndPush (TableHandler board) { //check if i can do a steal&push
+        int i, seed, j;
+        ArrayList <Integer>  candidate = new ArrayList();
+        Container c = board.getContainerByIndex(0);
+        for (i=0; i<14; i++) {
+            if (c.getOwnerId().equals(this.getId())) {
+                seed=c.getNumSeeds();
+                Container temp = c;
+                for (j=0; j< seed; j++)
+                    temp = temp.getNextContainer();
 
-    }
+                if (temp.getNumSeeds()==0 && temp.getOwnerId().equals(this.getId()))
+                    candidate.add(c.getIndex());
+
+
+            }
+        }
+
+        if (candidate.isEmpty()) {
+            return -1;
+        }
+            else {
+                ArrayList <Integer> numOppositeSeeds = new ArrayList();
+            for (i=0; i<candidate.size(); i++)
+                numOppositeSeeds.add(i, board.getNumOfSeedByIndex(12 - candidate.get(i)));
+            Integer max =0;
+            int index =0;
+            for (i=0; i< numOppositeSeeds.size(); i++)
+                if (numOppositeSeeds.get(i) >= max ) {
+                    max = numOppositeSeeds.get(i);
+                    index = i;
+                }
+            return candidate.get(index);
+        }
 }
