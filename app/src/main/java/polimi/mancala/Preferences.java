fcc82268bb9289db804a0239006d3326dbc4d74e
Mercurion <jackbianchini@gmail.com>
2015-01-13 17:03:52 +0100

    Giacomo Bianchini
    Finished the computer class. used in the definition of a H-C game.
    Created a class for the preferences.

--- app/src/main/java/polimi/mancala/Preferences.java
+++ app/src/main/java/polimi/mancala/Preferences.java
@@ -1,12 +1,13 @@
 package polimi.mancala;
 
 /**
- * Created by jack on 13/01/2015.
+ * Created by Giacomo Bianchini on 13/01/2015.
  */
 public class Preferences {
 
     private static Preferences instance;
     private boolean humanComputerGame;
+    private String userName;
 
     public static synchronized Preferences getPreferences () {
         if (instance == null)
@@ -21,5 +22,13 @@ public class Preferences {
 
     public void setHumanComputerGame(boolean humanComputerGame) {
         this.humanComputerGame = humanComputerGame;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 }
