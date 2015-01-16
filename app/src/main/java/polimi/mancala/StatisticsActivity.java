710577b3257475bae41f400f544d389998be773e
Mercurion <jackbianchini@gmail.com>
2015-01-16 18:19:02 +0100

    Giacomo: minor commit

--- app/src/main/java/polimi/mancala/StatisticsActivity.java
+++ app/src/main/java/polimi/mancala/StatisticsActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
@@ -20,38 +21,33 @@ import android.widget.TextView;
 public class StatisticsActivity extends Activity {
 
 
+    private Integer maxScore;
+    private Integer gamePlayed;
+    Statistics stat;
+
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        Button goHome = (Button) findViewById(R.id.goHomeButton);
-        final Button animButt = (Button) findViewById(R.id.animButton);
-        final TextView text = (TextView) findViewById(R.id.textView);
-        int x = 0;
-//        x = text.getHeight();
-        int y = 0;
-//        y = text.getWidth();
-        final Animation traslation = new TranslateAnimation(x, x+100, y, y+100);
-        traslation.setDuration(2000);
-        traslation.getFillAfter();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        animButt.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Do something in response to button click
-                text.startAnimation(traslation);
-            }
-        });
+        Button goHome = (Button) findViewById(R.id.goHomeButton);
+        final TextView text = (TextView) findViewById(R.id.textView);
+
         goHome.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Do something in response to button click
                 goBack(v);
             }
         });
     }
 
+
     public void goBack (View v) {
         Intent comeback = new Intent(this, MainActivity.class);
         startActivity(comeback);
+    }
 
+    public void reset () {
+        stat.resetAll();
     }
 }
