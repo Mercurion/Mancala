b9cfb14001b1739547ed7bce1af5244e37e222bf
Mercurion <jackbianchini@gmail.com>
2015-01-16 17:01:30 +0100

    Giacomo: minor commit

--- app/src/main/java/polimi/mancala/Statistics.java
+++ app/src/main/java/polimi/mancala/Statistics.java
@@ -4,32 +4,33 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Environment;
 
-import java.io.File;
 
 /**
  * @author Giacomo bianchini
  */
 public class Statistics {
 
-    File path;
     private SharedPreferences prefs;
-
-
-    public void Statistics (Context ctx) {
-        prefs = ctx.getSharedPreferences("", Context.MODE_PRIVATE); //todo: set the string to app name
-    }
-
     public int maxScore;
     public int gamePlayed;
+    private String playedGame = "playedGame";
+    private String bestScore = "MaxScore";
 
-    public void setPrefs(String key, String value) {
+    public void Statistics (Context ctx) {
+        prefs = ctx.getSharedPreferences("Mancala", Context.MODE_PRIVATE); //todo: set the string to app name
+
+    }
+
+
+    public void setPrefs(String key, int value) {
         SharedPreferences.Editor prefsEditor = prefs.edit();
-        prefsEditor.putString(key, value);
+        prefsEditor.putInt(key, value);
         prefsEditor.commit();
     }
 
-    public String getPrefs(String key, String value) {
-        return prefs.getString(key, value);
+    public int getPrefs(String key) {
+        int value =0;
+        return prefs.getInt(key, value);
     }
 
     public void removePrefs(String key) {
@@ -38,8 +39,6 @@ public class Statistics {
         prefsEditor.commit();
     }
 
-    //TODO: perform setter and getter using a file
-
     public int getMax (){
         return this.maxScore;
     }
@@ -47,27 +46,21 @@ public class Statistics {
     //TODO: has to be done with a pathfile
     public void setMax (int value) {
         this.maxScore = value;
+        setPrefs(bestScore, maxScore);
     }
 
     public int getGamePlayed () {
-        return this.gamePlayed;
+        return getPrefs(playedGame);
     }
 
     public void setGamePlayed (int value) {
         this.gamePlayed = value;
+        setPrefs(playedGame, gamePlayed);
     }
 
     public void oneMorePlayed () {
-        //TODO: here has to be set that the game played value will be increased by one
+        this.gamePlayed++;
     }
 
-    /* Checks if external storage is available for read and write */
-    public boolean isExternalStorageWritable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
-        }
-        return false;
-    }
 
 }
