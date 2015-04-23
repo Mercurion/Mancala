a56e6624fd7d9551dbba54b5e6f76488bd123363
Mercurion <jackbianchini@gmail.com>
2015-04-23 19:15:33 +0200

    Giacomo: minor commit --- Completed the ai in logic part

--- app/src/main/java/mercurion/mancala/activities/MainActivity.java
+++ app/src/main/java/mercurion/mancala/activities/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+
 import mercurion.mancala.R;
 import mercurion.mancala.SettingsActivity;
 import mercurion.mancala.StatisticsActivity;
