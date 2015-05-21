4c4845c3657c05f3cea066f6c629c4100106750e
Mercurion <jackbianchini@gmail.com>
2015-05-22 00:35:45 +0200

    Giacomo: minor commit

---
+++ app/src/main/java/mercurion/mancala/database/Matches.java
@@ -0,0 +1,34 @@
+package mercurion.mancala.database;
+
+/**
+ * Created by jack on 21/05/2015.
+ */
+public class Matches {
+
+    int id;
+    String winner;
+
+    public Matches (int id, String winner) {
+        this.id = id;
+        this.winner = winner;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setWinner(String winner) {
+        this.winner = winner;
+    }
+
+
+
+}
